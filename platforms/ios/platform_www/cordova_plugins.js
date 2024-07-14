6e2752449bb5078689513139d6c2cc11f058217e
Manel Roca <manel.roca@meetinglawyers.com>
2024-07-14 19:05:44 +0200

    feat: update plugin to github url

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -11,6 +11,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
   ];
   module.exports.metadata = {
     "cordova-plugin-add-swift-support": "2.0.2",
-    "com-meetinglawyers-cordova": "1.0.2"
+    "com-meetinglawyers-cordova": "1.0.6"
   };
 });
